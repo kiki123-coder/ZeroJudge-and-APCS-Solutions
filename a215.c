b9bd9f645a9d6b0e0d53e45e9cba09e451aa0d1f
kiki123-coder <kiki.cheng1215@gmail.com>
2026-07-29 17:02:56 +0800

    Add files via upload

---
+++ a215.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    long long m, n;
+
+    while(scanf("%lld %lld", &n, &m) != EOF){
+
+        long long sum=0, run=0;
+
+        for(long long i=n; ; i++){
+            sum+=i;
+            run++;
+
+            if(sum>m)
+                break;
+        }
+
+        printf("%lld\n", run);
+    }
+
+    return 0;
+}
+
---
+++ a216.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+long long f(int n){
+            if(n == 1){
+                return 1;
+            }
+            return n+f(n-1);
+        }
+
+
+long long g(int n){
+            if(n == 1){
+                return 1;
+            }
+            return f(n)+g(n-1);
+        }
+int main(void)
+{
+    int n;
+    while(scanf("%d", &n) != EOF){
+        printf("%lld %lld\n", f(n), g(n));
+    }
+}
