1b37d117cd597ff3d36b70acd2e8a271558b0c67
kiki123-coder <kiki.cheng1215@gmail.com>
2026-08-03 11:03:48 +0800

    Update a147.c

--- a147.c
+++ a147.c
@@ -2,13 +2,13 @@
 int main(void)
 {
     int n;
-    while(scanf("%d",&n) != 0){
+    while(scanf("%d", &n) != 0){
 
         if(n == 0){
         return 0;
         }
 
-        for(int i=0; i<n; i++){
+        for(int i = 0; i < n; i++){
             if(i%7 != 0){
                 printf("%d ", i);
             }
