8d99c4d3a241ec9270b3a2bc12ab54304bc371d6
kiki123-coder <kiki.cheng1215@gmail.com>
2026-08-03 11:20:04 +0800

    Update 二維陣列2.c

--- 二維陣列2.c
+++ 二維陣列2.c
@@ -15,9 +15,11 @@ int main(void)
     arr[2][1] = 6;
     arr[2][2] = 5;
     for(int i = 0; i < 3; i++){
+        
         for(int j = 0; j < 3; j++){
             printf("%d ", arr[i][j]);
         }
+        
         printf("\n");
     }
 
