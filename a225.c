8ea27805cd27f3b0c9aae83ae94241ab8e85765f
kiki123-coder <kiki.cheng1215@gmail.com>
2026-08-03 11:09:38 +0800

    Update a225.c

--- a225.c
+++ a225.c
@@ -6,28 +6,28 @@ int main(void)
     while(scanf("%d", &n) != EOF){
         int arr[1000];
 
-        for(int i=0; i<n; i++){
+        for(int i = 0; i < n; i++){
             scanf("%d", &arr[i]);
         }
 
-        for(int k=0; k<n; k++){
-            for(int i=0; i<n-1; i++){
+        for(int k = 0; k < n; k++){
+            for(int i = 0; i < n - 1; i++){
 
-                if(arr[i]%10 > arr[i+1]%10){
-                    int b=0;
-                    b=arr[i];
-                    arr[i]=arr[i+1];
-                    arr[i+1]=b;
+                if(arr[i]%10 > arr[i + 1]%10){
+                    int b = 0;
+                    b = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = b;
                 }
 
 
-                else if(arr[i]%10 == arr[i+1]%10){
+                else if(arr[i]%10 == arr[i + 1]%10){
 
-                    if(arr[i] < arr[i+1]){
-                        int b=0;
-                        b=arr[i];
-                        arr[i]=arr[i+1];
-                        arr[i+1]=b;
+                    if(arr[i] < arr[i + 1]){
+                        int b = 0;
+                        b = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = b;
                     }
                 }
 
@@ -35,7 +35,7 @@ int main(void)
 
         }
 
-        for(int j=0; j<n; j++){
+        for(int j = 0; j < n; j++){
             printf("%d ", arr[j]);
         }
         printf("\n");
