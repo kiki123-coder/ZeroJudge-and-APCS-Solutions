75dfc2b6c954b2c04381f1470ea27908a3acd5a5
kiki123-coder <kiki.cheng1215@gmail.com>
2026-08-03 11:12:18 +0800

    Update a291.c

--- a291.c
+++ a291.c
@@ -8,35 +8,35 @@ int main(void)
         int run;
         scanf("%d", &run);
 
-        for(int i=0; i<run; i++){
+        for(int i = 0; i < run; i++){
             int test[4];
-            int A=0, B=0;
-            int used_test[4]={0};
-            int used_arr[4]={0};
+            int A = 0, B = 0;
+            int used_test[4] = {0};
+            int used_arr[4] = {0};
             scanf("%d %d %d %d", &test[0], &test[1], &test[2], &test[3]);
 
-            for(int j=0; j<4; j++){
+            for(int j = 0; j < 4; j++){
                 //A
                 if(test[j] == arr[j]){
-                    A=A+1;
-                    used_arr[j]=1;  //紀錄已使用過的arr跟test
-                    used_test[j]=1;
+                    A = A + 1;
+                    used_arr[j] = 1;  //紀錄已使用過的arr跟test
+                    used_test[j] = 1;
 
                 }
             }
 
-                for(int j=0; j<4; j++){
+                for(int j = 0; j < 4; j++){
 
                     if(used_test[j]){
                         continue;
                     }
 
-                for(int k=0; k<4; k++){
+                for(int k = 0; k < 4; k++){
                     //B
-                    if(test[j] == arr[k] && used_arr[k]==0){
-                        B=B+1;
-                        used_arr[k]=1;
-                        used_test[j]=1;
+                    if(test[j] == arr[k] && used_arr[k] == 0){
+                        B = B + 1;
+                        used_arr[k] = 1;
+                        used_test[j] = 1;
                         break;
                         }
 
