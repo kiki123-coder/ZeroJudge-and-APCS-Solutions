97c91ff8e6fe386fd1398d42be5bb779a429ef24
kiki123-coder <kiki.cheng1215@gmail.com>
2026-08-03 11:30:31 +0800

    Update 建表4.c

--- 建表4.c
+++ 建表4.c
@@ -2,14 +2,14 @@
 
 int main(void)
 {
-    int romanTable[256]={0};
-    romanTable['I']=1;
-    romanTable['V']=5;
-    romanTable['X']=10;
-    romanTable['L']=50;
-    romanTable['C']=100;
-    romanTable['D']=500;
-    romanTable['M']=1000;
+    int romanTable[256] = {0};
+    romanTable['I'] = 1;
+    romanTable['V'] = 5;
+    romanTable['X'] = 10;
+    romanTable['L'] = 50;
+    romanTable['C'] = 100;
+    romanTable['D'] = 500;
+    romanTable['M'] = 1000;
 
     char c;
     scanf("%c", &c);
