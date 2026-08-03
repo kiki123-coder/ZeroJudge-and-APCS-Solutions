9a094e4b1fd08c1b5bbdc4e3cd1ce6e1410befc9
kiki123-coder <kiki.cheng1215@gmail.com>
2026-08-03 11:17:44 +0800

    Update a524.c

--- a524.c
+++ a524.c
@@ -2,14 +2,18 @@
 
 int n;
 int arr[9], used[9];
+
 void dfs(int step)
 {
     if(step == n){
+        
         for(int i = 0; i < n; i++){
             printf("%d", arr[i]);
         }
+        
         printf("\n");
     }
+    
     for(int i = n; i >= 1; i--){
         if(used[i] == 0){
             used[i] = 1;
@@ -27,7 +31,7 @@ void dfs(int step)
 int main(void)
 {
     while(scanf("%d", &n) != EOF){
-            dfs(0);
+        dfs(0);
         printf("\n");
     }
 }
