f7109bc7f94713c83073e144aacc4b5cd8c03b87
kiki123-coder <kiki.cheng1215@gmail.com>
2026-08-03 11:15:54 +0800

    Update a417.c

--- a417.c
+++ a417.c
@@ -5,39 +5,39 @@ int main(void)
     int T, N, M;
     scanf("%d", &T);
 
-    for(int i=0; i<T; i++){
+    for(int i = 0; i < T; i++){
         scanf("%d %d", &N, &M);
         int arr[100][100];
-        int top=0;
-        int left=0;
-        int right=N-1;
-        int bottom=N-1;
-        int num=1;
+        int top = 0;
+        int left = 0;
+        int right = N - 1;
+        int bottom = N - 1;
+        int num = 1;
 
-        if(M==1){
-            while(top<=bottom && left<=right){
+        if(M == 1){
+            while(top <= bottom && left <= right){
 
                 //往右
-                for(int i=left; i<=right; i++){
-                    arr[top][i]=num;
+                for(int i = left; i <= right; i++){
+                    arr[top][i] = num;
                     num++;
                 }
 
                 //往下
-                for(int i=top+1; i<=bottom; i++){
-                    arr[i][right]=num;
+                for(int i = top + 1; i <= bottom; i++){
+                    arr[i][right] = num;
                     num++;
                 }
 
                 //往左
-                for(int i=right-1; i>=left; i--){
-                    arr[bottom][i]=num;
+                for(int i = right - 1; i >= left; i--){
+                    arr[bottom][i] = num;
                     num++;
                 }
 
                 //往上
-                for(int i=bottom-1; i>top; i--){
-                    arr[i][left]=num;
+                for(int i = bottom - 1; i > top; i--){
+                    arr[i][left] = num;
                     num++;
                 }
                 top++;
@@ -48,30 +48,30 @@ int main(void)
             }
         }
 
-        else if(M==2){
-            while(top<=bottom && left<=right){
+        else if(M == 2){
+            while(top <= bottom && left <= right){
 
                 //往下
-                for(int i=top; i<=bottom; i++){
-                    arr[i][left]=num;
+                for(int i = top; i <= bottom; i++){
+                    arr[i][left] = num;
                     num++;
                 }
 
                 //往右
-                for(int i=left+1; i<=right; i++){
-                    arr[bottom][i]=num;
+                for(int i = left + 1; i <= right; i++){
+                    arr[bottom][i] = num;
                     num++;
                 }
 
                 //往上
-                for(int i=bottom-1; i>=top; i--){
-                    arr[i][right]=num;
+                for(int i = bottom - 1; i >= top; i--){
+                    arr[i][right] = num;
                     num++;
                 }
 
                 //往左
-                for(int i=right-1; i>left; i--){
-                    arr[top][i]=num;
+                for(int i = right - 1; i > left; i--){
+                    arr[top][i] = num;
                     num++;
                 }
 
@@ -86,8 +86,8 @@ int main(void)
 
 
 
-        for(int k=0; k<N; k++){
-            for(int j=0; j<N; j++){
+        for(int k = 0; k < N; k++){
+            for(int j = 0; j < N; j++){
                 printf("%5d", arr[k][j]);
             }
             printf("\n");
