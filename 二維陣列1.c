fcda2357c75736769d52e682e911cdf21d896148
kiki123-coder <kiki.cheng1215@gmail.com>
2026-08-03 11:20:27 +0800

    Update 二維陣列1.c

--- 二維陣列1.c
+++ 二維陣列1.c
@@ -5,16 +5,20 @@ int main(void)
     int arr[3][3];
     int num = 1;
         for(int j = 0; j < 3; j++){
+            
             for(int k = 0; k < 3; k++){
                 arr[j][k] = num;
                 num++;
             }
+            
         }
 
         for(int i = 0; i < 3; i++){
+            
             for(int j = 0; j < 3; j++){
                 printf("%d ", arr[i][j]);
             }
+            
             printf("\n");
         }
 
