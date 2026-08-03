7922effb45cacc2fba470b1aac10da20f8266613
kiki123-coder <kiki.cheng1215@gmail.com>
2026-08-03 11:16:38 +0800

    Update a524.c

--- a524.c
+++ a524.c
@@ -4,20 +4,20 @@ int n;
 int arr[9], used[9];
 void dfs(int step)
 {
-    if(step==n){
-        for(int i=0; i<n; i++){
+    if(step == n){
+        for(int i = 0; i < n; i++){
             printf("%d", arr[i]);
         }
         printf("\n");
     }
-    for(int i=n; i>=1; i--){
+    for(int i = n; i >= 1; i--){
         if(used[i] == 0){
-            used[i]=1;
-            arr[step]=i;
+            used[i] = 1;
+            arr[step] = i;
 
-            dfs(step+1);
+            dfs(step + 1);
 
-            used[i]=0;
+            used[i] = 0;
         }
     }
     return;
